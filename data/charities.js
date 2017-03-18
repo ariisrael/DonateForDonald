6b1ee6a674c4b6dc95372e7501bb67009388728e
Ari Israel <aei225@nyu.edu>
2017-03-18 17:28:18 -0400

    removed 501c4s and deductible property

--- data/charities.js
+++ data/charities.js
@@ -35,12 +35,6 @@ module.exports = {
         twitter: ['@ADL_National'],
         deductible: true
     }, {
-        ein: '20-8802884',
-        name: 'Everytown For Gun Safety Action Fund',
-        site: 'www.everytown.org',
-        twitter: ['@Everytown'],
-        deductible: false
-    }, {
         ein: '95-4539765',
         name: 'National Immigration Law Center',
         alias: ['NILC'],
@@ -57,225 +51,191 @@ module.exports = {
     }, {
         ein: '13-1655255',
         name: 'NAACP Legal Defense and Educational Fund',
-        alias: ['NAACP LDF'],
+        alias: ['LDF'],
         site: 'www.naacpldf.org',
-        twitter: ['@NAACP_LDF', '@NAACP'],
-        deductible: true
+        twitter: ['@NAACP_LDF', '@NAACP']
     }, {
         ein: '77-0646756',
         name: 'Council on American-Islamic Relations Foundation',
         alias: ['CAIR'],
         site: 'www.cair.org',
-        twitter: ['@CAIRNational'],
-        deductible: true
+        twitter: ['@CAIRNational']
     }, {
         ein: '04-3091431',
         name: 'Electronic Freedom Foundation',
         alias: 'EFF',
         site: 'www.eff.org',
-        twitter: '@EFF',
-        deductible: true
+        twitter: ['@EFF']
     }, {
         ein: '06-0726487',
         name: 'Save the Children',
         site: 'www.savethechildren.org',
-        twitter: ['@SaveTheChildren'],
-        deductible: true
+        twitter: ['@SaveTheChildren']
     }, {
         ein: '52-1343924',
         name: 'Government Accountability Project',
         alias: ['GAP'],
         twitter: ['@GovAcctProj'],
-        site: 'www.whistleblower.org',
-        deductible: true
+        site: 'www.whistleblower.org'
     }, {
         ein: '32-0077563',
         twitter: ['@innocence'],
         name: 'Innocence Project',
-        site: 'www.innocenceproject.org',
-        deductible: true
+        site: 'www.innocenceproject.org'
     }, {
         ein: '14-2007220',
         name: 'ProPublica',
         site: 'www.propublica.org',
-        twitter: ['@ProPublica'],
-        deductible: true
+        twitter: ['@ProPublica']
     }, {
         ein: '52-1388917',
         name: 'Conservation Fund',
         site: 'www.conservationfund.org',
-        twitter: ['@ConservationFnd'],
-        deductible: true
+        twitter: ['@ConservationFnd']
     }, {
         ein: '13-3081500',
         name: 'Committee to Protect Journalists',
         alias: ['CPJ'],
         site: 'www.cpj.org',
-        twitter: ['@pressfreedom'],
-        deductible: true
+        twitter: ['@pressfreedom']
     }, {
         ein: '53-0196605',
         name: 'American Red Cross',
         site: 'www.redcross.org',
-        twitter: ['@RedCross'],
-        deductible: true
+        twitter: ['@RedCross']
     }, {
         ein: '31-1580204',
         name: 'Clinton Foundation',
         alias: ['Clinton Global Initiative', 'CF'],
         twitter: ['@ClintonFdn', '@HillaryClinton'],
-        site: 'www.clintonfoundation.org',
-        deductible: true
+        site: 'www.clintonfoundation.org'
     }, {
         ein: '13-1760110',
         twitter: ['@UNICEF'],
         name: 'United States Fund for UNICEF',
         alias: ['UNICEF'],
-        site: 'www.unicefusa.org',
-        deductible: true
+        site: 'www.unicefusa.org'
     }, {
         ein: '20-2370934',
         name: 'Wounded Warrior Project',
         site: 'www.woundedwarriors.org',
         alias: ['WWP'],
-        twitter: ['@WWP'],
-        deductible: true
+        twitter: ['@WWP']
     }, {
         ein: '63-0598743',
         name: 'Southern Poverty Law Center',
         alias: ['SPLC'],
         site: 'https://www.splcenter.org/',
-        twitter: ['@splcenter'],
-        deductible: true
+        twitter: ['@splcenter']
     }, {
         ein: '36-3241033',
         name: 'American Refugee Committee',
         alias: 'ARC',
         site: 'www.arcrelief.org',
-        twitter: '@ARCrelief',
-        deductible: true
+        twitter: '@ARCrelief'
     }, {
         ein: '52-1477004',
         name: 'National Organization for Women Foundation',
         alias: ['NOW'],
         site: 'www.now.org',
-        twitter: ['@NationalNOW'],
-        deductible: true
+        twitter: ['@NationalNOW']
     }, {
         ein: '46-2216565',
         name: 'United We Dream',
         site: 'http://unitedwedream.org/',
         alias: ['UWD'],
-        twitter: ['@UNITEDWEDREAM'],
-        deductible: true       
+        twitter: ['@UNITEDWEDREAM']       
     }, {
         ein: '52-1662800',
         name: 'United Nations High Commissioner for Refugees',
         site: 'http://www.unrefugees.org/',
         alias: ['UNHCR', 'UN Refugee Agency', 'USA for UNHCR'],
-        twitter: ['@Refugees', '@unhcr', '@UNRefugeeAgency'],
-        deductible: true       
+        twitter: ['@Refugees', '@unhcr', '@UNRefugeeAgency']
     }, {
         ein: '95-4681287',
         name: 'The Trevor Project',
         site: 'http://www.thetrevorproject.org/',
-        twitter: ['@TrevorProject'],
-        deductible: true      
+        twitter: ['@TrevorProject']    
     }, {
         ein: '77-0071852',
         name: 'Black Lives Matter',
         site: 'http://blacklivesmatter.com/',
         alias: ['BLM', 'IDEX'],
-        twitter: '@Blklivesmatter',
-        deductible: true   
+        twitter: '@Blklivesmatter'
     }, {
-        ein: '94-1153307',
-        name: 'Sierra Club',
-        site: 'http://www.sierraclub.org/',
+        ein: '94-6069890',
+        name: 'Sierra Club Foundation',
+        site: 'http://www.sierraclubfoundation.org',
         alias: ['SCF'],
-        twitter: ['@SierraClub'],
-        deductible: false 
+        twitter: ['@SierraClub']
     }, {
         ein: '13-2875808',
         name: 'Human Rights Watch',
         site: 'http://www.hrw.org/',
         alias: ['HRW'],
-        twitter: ['@hrw'],
-        deductible: true 
+        twitter: ['@hrw']
     }, {
         ein: '52-0851555',
         name: 'Amnesty International USA',
         site: 'www.amnestyusa.org/',
-        twitter: ['@amnesty'],
-        deductible: true
+        twitter: ['@amnesty']
     }, {
         ein: '74-1563270',
         name: 'Mexican American Legal Defense and Educational Fund',
         alias: ['MALDEF'],
         site: 'www.maldef.org/',
-        twitter: ['@MALDEF'],
-        deductible: true
+        twitter: ['@MALDEF']
     }, {
         ein: '04-2535767',
         name: 'Union of Concerned Scientists',
         alias: ['UCSUSA'],
         site: 'www.ucsusa.org/',
-        twitter: ['@UCSUSA'],
-        deductible: true
+        twitter: ['@UCSUSA']
     }, {
         ein: '11-6107128',
         name: 'Environmental Defense Fund',
         alias: ['EDF'],
         site: 'https://www.edf.org/',
-        twitter: ['@EnvDefenseFund'],
-        deductible: true
+        twitter: ['@EnvDefenseFund']
     }, {
         ein: '13-3447888',
         name: 'PEN America',
         site: 'https://pen.org/',
-        twitter: ['@PENamerican'],
-        deductible: true
+        twitter: ['@PENamerican']
     }, {
         ein: '95-3313195',
         name: 'Greenpeace Fund',
         site: 'http://greenpeacefund.org/',
-        twitter: ['@Greenpeace'],
-        deductible: true
+        twitter: ['@Greenpeace']
     }, {
         ein: '52-1218336',
         name: 'People for the Ethical Treatment of Animals',
         alias: ['PETA'],
         site: 'http://www.peta.org/',
-        twitter: ['@peta'],
-        deductible: true        
+        twitter: ['@peta']     
     }, {
         ein: '52-1693387',
         name: 'World Wildlife Fund',
         alias: ['WWF', 'World Wide Fund for Nature'],
         site: 'https://www.worldwildlife.org/',
-        twitter: ['@WWF'],
-        deductible: true        
+        twitter: ['@WWF']    
     }, {
         ein: '13-3433452',
         name: 'Doctors Without Borders',
         alias: ['MSF', 'Médecins Sans Frontières'],
         site: 'http://www.doctorswithoutborders.org/',
-        twitter: ['@MSF_USA', '@MSF'],
-        type: '',
-        deductible: true 
+        twitter: ['@MSF_USA', '@MSF']
     }, {
         ein: '13-5660870',
         name: 'International Rescue Committee',
         alias: ['IRC'],
         site: 'https://www.rescue.org/',
-        twitter: ['@theIRC'],
-        deductible: true 
+        twitter: ['@theIRC']
     }, {
         ein: '35-1019477',
         name: 'Heifer Project International',
         site: 'https://www.heifer.org/',
-        twitter: ['@heifer'],
-        deductible: true 
+        twitter: ['@heifer']
     }, {
         ein: '91-1148123',
         name: 'Mercy Corps',
@@ -286,20 +246,17 @@ module.exports = {
         ein: '23-7069110',
         name: 'Oxfam America',
         site: 'https://www.oxfamamerica.org/',
-        twitter: ['@Oxfam', '@OxfamAmerica'],
-        deductible: true 
+        twitter: ['@Oxfam', '@OxfamAmerica']
     }, {
         ein: '‎46-4950751',
         name: 'Obama Foundation',
         site: 'https://www.obama.org/',
-        twitter: ['@ObamaFoundation', '@BarackObama'],
-        deductible: true 
+        twitter: ['@ObamaFoundation', '@BarackObama']
     }, {
         ein: '13-5563422',
         name: 'Catholic Relief Services',
         alias: ['CRS'],
         site: 'www.crs.org/',
-        twitter: ['@catholicrelief'],
-        deductible: true 
+        twitter: ['@catholicrelief']
     }
 ]};
