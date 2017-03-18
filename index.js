f226e43c669e42329812cf6d2b7b0bba34021099
Ari Israel <aei225@nyu.edu>
2017-03-18 16:26:50 -0400

    linted index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const dotenv = require('dotenv');
-const express = require('express')
-const morgan = require('morgan')
-const favicon = require('serve-favicon')
-const path = require('path')
+const express = require('express');
+const morgan = require('morgan');
+const favicon = require('serve-favicon');
+const path = require('path');
 const expressValidator = require('express-validator');
 const exphbs = require('express-handlebars');
 const flash = require('express-flash');
@@ -14,17 +14,17 @@ if (process.env.NODE_ENV !== 'production') {
   dotenv.config();
 }
 
-var app = express();
+const app = express();
 
-module.exports = exports = app
+module.exports = app;
 
 app.use(morgan('combined'));
 app.use(compression());
 app.use(expressValidator());
 app.use(methodOverride('_method'));
 app.use(flash());
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -35,16 +35,16 @@ app.set('views', './views');
 const hbs = exphbs.create({
   defaultLayout: 'main',
   helpers: {
-    ifeq: function(a, b, options) {
+    ifeq(a, b, options) {
       if (a === b) {
         return options.fn(this);
       }
       return options.inverse(this);
     },
-    toJSON : function(object) {
+    toJSON(object) {
       return JSON.stringify(object);
-    }
-  }
+    },
+  },
 });
 
 app.engine('handlebars', hbs.engine);
@@ -54,6 +54,4 @@ app.set('view engine', 'handlebars');
 require('./config/web');
 require('./routes');
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
+app.listen(app.get('port'), () => `Node app is running on port ${app.get('port')}`);
