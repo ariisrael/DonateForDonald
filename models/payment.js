32cc76bfd6216688de808d0d62ededdad61360b7
Ari Israel <aei225@nyu.edu>
2017-03-23 19:34:44 -0400

    removed redundant date

--- models/payment.js
+++ models/payment.js
@@ -4,7 +4,6 @@ const Schema = mongoose.Schema;
 const ObjectId = mongoose.ObjectId;
 
 const PaymentSchema = new Schema({
-    date: Date,
     charityId: ObjectId,
     triggerId: ObjectId,
     userId: ObjectId,
