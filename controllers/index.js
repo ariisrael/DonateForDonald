ad46082cd39f4588c037762b92e3c679de122375
Ari Israel <aei225@nyu.edu>
2017-03-23 20:47:58 -0400

    add back home controller

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,12 @@
 // Export all controllers
 
+// Models
 exports.charities = require('./charities');
 exports.triggers = require('./triggers');
 exports.users = require('./users');
 exports.donations = require('./donations');
 exports.tweets = require('./tweets');
 exports.payments = require('./payments');
+
+// Pages
+exports.home = require('./home');
