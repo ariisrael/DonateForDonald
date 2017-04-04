fe7913ed10c5555228497c0e6b00d046ce1d561a
Greg Meyer <greg.meyer@datadoghq.com>
2017-04-03 22:18:56 -0400

    match phrase

--- controllers/tweets.js
+++ controllers/tweets.js
@@ -25,7 +25,7 @@ module.exports = {
         return res.json([])
       }
       Tweet.find(
-        { $text : { $search : query } },
+        { $text : { $search : '"' + query + '""' } },
         { score : { $meta: "textScore" } }
       )
       .sort({ score : { $meta : 'textScore' } })
