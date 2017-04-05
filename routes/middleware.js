cab991b895b401d60ba10063ae6655ab1498ab4a
Greg Meyer <greg.meyer@datadoghq.com>
2017-04-05 09:02:02 -0400

    enable cors

--- routes/middleware.js
+++ routes/middleware.js
@@ -45,3 +45,9 @@ app.use(function(req, res, next) {
   res.locals.env = process.env.NODE_ENV
   next()
 })
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
