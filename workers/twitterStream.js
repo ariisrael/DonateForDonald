9c0e5820a7a96fa329b909a2acb5146936dbface
Greg Meyer <greg.meyer@datadoghq.com>
2017-04-06 23:40:13 -0400

    turns down logging

--- workers/twitterStream.js
+++ workers/twitterStream.js
@@ -19,9 +19,9 @@ var stream = T.stream('statuses/filter', { follow: TRUMP_USER_ID});
 // Connect to Twitter API and start streaming
 stream.on('tweet', function (tweet) {
   // Only parse tweets from @realDonaldTrump
-  console.log("new tweet: ", getFullText(tweet))
-  console.log("matches trump? ", tweet.user.id === TRUMP_USER_ID)
   if (tweet.user.id == TRUMP_USER_ID) {
+    console.log("new tweet: ", getFullText(tweet))
+    console.log("matches trump!")
     var text = getFullText(tweet);
     var id = tweet.id_str;
     var date = tweet.created_at;
