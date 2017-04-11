d83df27b162929848c22877927c2b47bc8cfc9f7
Ari Israel <aei225@nyu.edu>
2017-04-10 21:17:04 -0400

    removed google routes

--- routes/index.js
+++ routes/index.js
@@ -85,8 +85,5 @@ app.get('/auth/facebook/callback', function(req, res, next) {
   })(req, res, next);
 });
 
-app.get('/auth/google', passport.authenticate('google', { scope: 'profile email' }));
-app.get('/auth/google/callback', passport.authenticate('google', { successRedirect: '/', failureRedirect: '/login' }));
-
 app.get('/auth/twitter', passport.authenticate('twitter'));
 app.get('/auth/twitter/callback', passport.authenticate('twitter', { successRedirect: '/triggers', failureRedirect: '/login' }));
