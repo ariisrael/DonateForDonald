400fd518e7baedad75e64182df3400b67c21855f
Greg Meyer <greg.meyer@datadoghq.com>
2017-04-11 20:05:57 -0400

    populates charities

--- utils/seed/donations.js
+++ utils/seed/donations.js
@@ -1,5 +1,5 @@
 if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
+    require('dotenv').config()
 }
 
 var config = require('../../config/worker')
@@ -12,35 +12,35 @@ const Donation = models.Donation
 const User = models.User
 
 db.once('open', function() {
-  var date = new Date();
-  var firstDayOfMonth = new Date(date.getFullYear(), date.getMonth(), 1);
-  User.find((err, users) => {
-    if (err) {
-      console.log('trigger finding err', err)
-    }
-    Tweet.find().limit(10).exec((err, tweets) => {
-      if (err) {
-        console.log('trigger finding err', err)
-      }
-      users.forEach((user) => {
-        Trigger.find({ userId: user.id }).exec((err, triggers) => {
-          if (err) {
+    var date = new Date();
+    var firstDayOfMonth = new Date(date.getFullYear(), date.getMonth(), 1);
+    User.find((err, users) => {
+        if (err) {
             console.log('trigger finding err', err)
-          }
-          triggers.forEach((trigger) => {
-            tweets.forEach((tweet) => {
-              var d = new Donation({
-                userId: user.id,
-                triggerId: trigger.id,
-                amount: trigger.amount,
-                charityId: trigger.charityId.id,
-                tweetId: tweet.id
-              })
-              console.log(d)
+        }
+        Tweet.find().limit(10).exec((err, tweets) => {
+            if (err) {
+                console.log('trigger finding err', err)
+            }
+            users.forEach((user) => {
+                Trigger.find({userId: user.id}).populate('charityId').exec((err, triggers) => {
+                    if (err) {
+                        console.log('trigger finding err', err)
+                    }
+                    triggers.forEach((trigger) => {
+                        tweets.forEach((tweet) => {
+                            var d = new Donation({
+                              userId: user.id, 
+                              triggerId: trigger.id,
+                              amount: trigger.amount,
+                              charityId: trigger.charityId.id,
+                              tweetId: tweet.id
+                            })
+                            console.log(d)
+                        })
+                    })
+                })
             })
-          })
         })
-      })
     })
-  })
 })
