1219102bff7aa6adb78ac103770afbbd6d21858d
Ari Israel <aei225@nyu.edu>
2017-04-13 17:38:31 -0400

    cleaned up triggers page

--- client/js/main.js
+++ client/js/main.js
@@ -525,6 +525,56 @@ function noLinkReload() {
   });
 }
 
+$(document).ready(function () {
+  function updateTrigger(id, data, callback) {
+    var url = '/api/triggers/' + id
+    jQuery.ajax({
+      type: 'put',
+      url: url,
+      data: data,
+    })
+    .done(function(data, textStatus, jqXHR) {
+      callback(null, data)
+    })
+    .fail(function(data, textStatus, errorThrown) {
+      callback(errorThrown, data)
+    })
+  }
+
+  $('.js-share-button').popup();
+
+  $('.js-edit-button').on('click', function () {
+    $('.ui.modal').modal('show');
+  });
+
+  $('.js-trigger-item .js-active-button').on('click', function(evt) {
+    var self = this;
+    var id = $(this).data('id')
+    $(this).find('.icon').toggleClass('hide')
+    $(this).toggleClass('disabled')
+    var data = {}
+    if ($(this).hasClass('js-disable')) {
+      data.active = false
+    } else {
+      data.active = true
+    }
+
+    updateTrigger(id, data, function(error, data) {
+      var classList = '.js-trigger-id-' + id + ' .js-active-button'
+      if (!error) {
+        $(classList).toggleClass('hide')
+      }
+      $(self).find('.icon').toggleClass('hide')
+      $(self).toggleClass('disabled')
+    })
+  });
+
+  $('.js-close-nag').on('click', function () {
+    $('.js-email-nag').css('display', 'none');
+  });
+})
+
+
 var whitelist = ['Ocare', 'Chicago', 'inner cities', 'sad', 'total scam', 'Obama', 'Jobs, Jobs, Jobs', 'ratings', 'The Apprentice', 'unmasking'];
 var blacklist = ['washinon', 'jobs jobs jobs'];
 $('.main-landing').ready(function () {
