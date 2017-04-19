66034d9c88c053d3d5477fdd79c8474c4b492c4f
Greg Meyer <greg.meyer@datadoghq.com>
2017-04-19 19:54:54 -0400

    should fix payment form on settings page

---
+++ client/js/panda.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+  // only initialize it if it should be initialized
+  if ($('.payment-page').length || $('.settings-page').length) {
+    Panda.init('pk_test_DGoDgSY4_9eNPYwXWQyNKg', 'payment-form');
+
+    Panda.on('success', function (cardToken) {
+      // You now have a token you can use to refer to that credit card later.
+      // This token is used in PandaPay API calls for creating donations and grants
+      // so that you don't have to worry about security concerns with dealing with
+      // credit card data.
+      var data = { paymentToken: cardToken }
+      if($('.js-maximum-amount input').val() && $('.js-maximum-amount input').val() !== 0 ) {
+        var maximum = $('.js-maximum-amount input').val();
+        data.monthlyLimit = maximum;
+      }
+      jQuery.ajax({
+        type: 'put',
+        url: '/api/users/' + user.id,
+        data: data,
+        success: function() {
+          window.location.replace("/social");
+        },
+        dataType: 'json'
+      })
+    });
+
+    Panda.on('error', function (errors) {
+      // errors is a human-readable list of things that went wrong
+      //  (invalid card number, missing last name, etc.)
+      console.log(errors);
+    });
+  }
+})
