78aef09ac334db58b884ad99187434da3fe62e2d
Greg Meyer <greg.meyer@datadoghq.com>
2017-04-29 22:31:16 -0400

    changes donation model to remove uniqueness index

--- models/donation.js
+++ models/donation.js
@@ -8,22 +8,10 @@ const DonationSchema = new Schema({
   userId: ObjectId,
   triggerId: { type: ObjectId, ref: 'Trigger'},
   charityId: { type: String, ref: 'Charity'},
-  uniqueness: {
-    type: String,
-    unique: true
-  },
   amount: { type: Number, min: 0, required: true },
   tweetId: { type: String, ref: 'Tweet' },
   paid: { type: Boolean, default: false },
 });
-
-DonationSchema.pre('validate', function(next) {
-  // Each user should only donate once for each tweet
-  // So, there's a unique: true on uniqueness, which is the hash of the userId and tweetId
-  var uniqueness = this.userId + this.tweetId;
-  this.uniqueness = crypto.createHash('md5').update(uniqueness).digest('hex')
-  next()
-})
 
 const Donation = mongoose.model('Donation', DonationSchema);
 module.exports = Donation;
